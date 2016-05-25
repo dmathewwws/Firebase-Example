c05f5eb27f984a20b6c9001ef42c060b53097e4f
Daniel Mathews <dannyl.mathews@gmail.com>
2016-05-25 15:00:51 -0700

    starter

---
+++ Firebase-Test-App/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Firebase-Test-App
+//
+//  Created by Daniel Mathews on 2016-05-25.
+//  Copyright © 2016 Daniel Mathews. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+use_frameworks!
+
+target "Firebase-Test-App" do
+pod 'Firebase/Core'
+pod 'Firebase/Database'
+end
